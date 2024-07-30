4ed3574e8806301097b67b0a1702e0c2707db671
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-07-30 15:43:14 +0900

    .

--- src/bio_data_to_db/utils/postgresql.py
+++ src/bio_data_to_db/utils/postgresql.py
@@ -1,37 +1,72 @@
-from __future__ import annotations
-
 import logging
 from collections.abc import Sequence
 
 import polars as pl
 import psycopg
-from polars.type_aliases import DbWriteEngine, DbWriteMode
+from polars._typing import DbWriteMode
 from psycopg import sql
+from sqlalchemy import Engine, create_engine, types
 
 logger = logging.getLogger(__name__)
 
 
-def polars_datatype_to_postgresql_type(dtype: pl.datatypes.DataType) -> str:
+# def polars_datatype_to_postgresql_type(dtype: pl.datatypes.DataType) -> str:
+#     match dtype:
+#         case pl.Utf8:
+#             pg_element_type = "text"
+#         case pl.UInt64:
+#             # WARN: ignore unsigned because PostgreSQL doesn't support unsigned
+#             pg_element_type = "bigint"
+#         case pl.Int64:
+#             pg_element_type = "bigint"
+#         case pl.Int32:
+#             pg_element_type = "integer"
+#         case pl.Int16:
+#             pg_element_type = "smallint"
+#         case pl.Float64:
+#             pg_element_type = "double precision"
+#         case pl.Float32:
+#             pg_element_type = "real"
+#         case _:
+#             raise ValueError(f"Unsupported inner dtype: {dtype}")
+#
+#     return pg_element_type
+
+
+def polars_datatype_to_sqlalchemy_type(
+    dtype: pl.datatypes.DataType,
+) -> types.TypeEngine:
     match dtype:
         case pl.Utf8:
-            pg_element_type = "text"
+            sqlalchemy_type = types.TEXT()
         case pl.UInt64:
-            # WARN: ignore unsigned because PostgreSQL doesn't support unsigned
-            pg_element_type = "bigint"
+            # WARN: ignore unsigned because PostgreSQL doesn't support unsigned. Possible loss of data
+            sqlalchemy_type = types.BIGINT()
         case pl.Int64:
-            pg_element_type = "bigint"
+            sqlalchemy_type = types.BIGINT()
+        case pl.UInt32:
+            sqlalchemy_type = types.BIGINT()
         case pl.Int32:
-            pg_element_type = "integer"
+            sqlalchemy_type = types.INTEGER()
+        case pl.UInt16:
+            sqlalchemy_type = types.INTEGER()
         case pl.Int16:
-            pg_element_type = "smallint"
+            sqlalchemy_type = types.SMALLINT()
         case pl.Float64:
-            pg_element_type = "double precision"
+            sqlalchemy_type = types.DOUBLE_PRECISION()
         case pl.Float32:
-            pg_element_type = "real"
+            sqlalchemy_type = types.REAL()
+        case pl.Boolean:
+            sqlalchemy_type = types.BOOLEAN()
+        case pl.List:
+            inner_dtype = dtype.inner
+            assert inner_dtype is not None
+            inner_sqlalchemy_type = polars_datatype_to_sqlalchemy_type(inner_dtype)
+            sqlalchemy_type = types.ARRAY(inner_sqlalchemy_type)
         case _:
-            raise ValueError(f"Unsupported inner dtype: {dtype}")
+            raise ValueError(f"Unsupported dtype: {dtype}")
 
-    return pg_element_type
+    return sqlalchemy_type
 
 
 def create_db_if_not_exists(uri_wo_db: str, db_name: str, comment: str | None = None):
@@ -338,75 +373,108 @@ def split_column_str_to_list(
             )
 
 
+# def polars_write_database(
+#     df: pl.DataFrame,
+#     *,
+#     schema_name: str = "public",
+#     table_name: str,
+#     connection: str,
+#     if_table_exists: DbWriteMode = "fail",
+#     engine: DbWriteEngine = "sqlalchemy",
+# ):
+#     """
+#     pl.DataFrame.write_database() but address the issue of writing unsigned and list columns to database.
+#
+#     Save all list columns as string columns, then split them back to list columns in the database.
+#     This takes some time.
+#     """
+#     # UInt64 to Int64
+#     for col in df.columns:
+#         if df[col].dtype == pl.UInt64:
+#             df = df.with_columns(pl.col(col).cast(pl.Int64).alias(col))
+#
+#     # List to string
+#     columns_with_list = [col for col in df.columns if df[col].dtype == pl.List]
+#     col_to_inner_dtype: dict[str, pl.datatypes.DataType] = {}
+#     for col in columns_with_list:
+#         dtype = df[col].dtype
+#         assert isinstance(dtype, pl.List)
+#         inner_dtype = dtype.inner
+#         assert inner_dtype is not None
+#         col_to_inner_dtype[col] = inner_dtype
+#
+#         df = df.with_columns(
+#             pl.col(col)
+#             .cast(pl.List(pl.Utf8))
+#             .list.join("/@#DSLKF")
+#             .alias(f"{col}_strjoinedAOIFDSIUH")
+#         )
+#         df.drop_in_place(col)
+#
+#     # Make safe table_name with schema_name.
+#     # pl.DataFrame.write_database() only has table_name, so we need to add schema_name.
+#     table_with_schema_str = None
+#     with psycopg.connect(
+#         conninfo=connection,
+#     ) as conn:
+#         cursor = conn.cursor()
+#         table_with_schema_str = (
+#             sql.SQL("""{table_name}""")
+#             .format(table_name=sql.Identifier(schema_name, table_name))
+#             .as_string(cursor)
+#         )
+#     if table_with_schema_str is None:
+#         raise ValueError(
+#             "Failed to get table name with schema. Maybe we can't connect to the database."
+#         )
+#
+#     df.write_database(
+#         table_name=table_with_schema_str,
+#         connection=connection,
+#         if_table_exists=if_table_exists,
+#         engine=engine,
+#     )
+#
+#     for col in columns_with_list:
+#         split_column_str_to_list(
+#             uri=connection,
+#             schema_name=schema_name,
+#             table_name=table_name,
+#             in_column=f"{col}_strjoinedAOIFDSIUH",
+#             out_column=col,
+#             separator="/@#DSLKF",
+#             pg_element_type=polars_datatype_to_postgresql_type(col_to_inner_dtype[col]),
+#         )
+
+
 def polars_write_database(
     df: pl.DataFrame,
     *,
     schema_name: str = "public",
     table_name: str,
-    connection: str,
+    connection: str | Engine,
     if_table_exists: DbWriteMode = "fail",
-    engine: DbWriteEngine = "sqlalchemy",
 ):
     """
     pl.DataFrame.write_database() but address the issue of writing unsigned and list columns to database.
 
-    Save all list columns as string columns, then split them back to list columns in the database.
-    This takes some time.
+    https://stackoverflow.com/questions/77098480/polars-psycopg2-write-column-of-lists-to-postgresql
     """
-    # UInt64 to Int64
-    for col in df.columns:
-        if df[col].dtype == pl.UInt64:
-            df = df.with_columns(pl.col(col).cast(pl.Int64).alias(col))
+    if isinstance(connection, str):
+        connection = create_engine(connection)
 
-    # List to string
-    columns_with_list = [col for col in df.columns if df[col].dtype == pl.List]
-    col_to_inner_dtype: dict[str, pl.datatypes.DataType] = {}
-    for col in columns_with_list:
-        dtype = df[col].dtype
-        assert isinstance(dtype, pl.List)
-        inner_dtype = dtype.inner
-        assert inner_dtype is not None
-        col_to_inner_dtype[col] = inner_dtype
+    columns_dtype = {col: df[col].dtype for col in df.columns}
 
-        df = df.with_columns(
-            pl.col(col)
-            .cast(pl.List(pl.Utf8))
-            .list.join("/@#DSLKF")
-            .alias(f"{col}_strjoinedAOIFDSIUH")
-        )
-        df.drop_in_place(col)
+    column_name_to_sqlalchemy_type = {
+        col: polars_datatype_to_sqlalchemy_type(dtype)
+        for col, dtype in columns_dtype.items()
+    }
 
-    # Make safe table_name with schema_name.
-    # pl.DataFrame.write_database() only has table_name, so we need to add schema_name.
-    table_with_schema_str = None
-    with psycopg.connect(
-        conninfo=connection,
-    ) as conn:
-        cursor = conn.cursor()
-        table_with_schema_str = (
-            sql.SQL("""{table_name}""")
-            .format(table_name=sql.Identifier(schema_name, table_name))
-            .as_string(cursor)
-        )
-    if table_with_schema_str is None:
-        raise ValueError(
-            "Failed to get table name with schema. Maybe we can't connect to the database."
-        )
-
-    df.write_database(
-        table_name=table_with_schema_str,
-        connection=connection,
-        if_table_exists=if_table_exists,
-        engine=engine,
+    df.to_pandas(use_pyarrow_extension_array=True).to_sql(
+        schema=schema_name,
+        name=table_name,
+        con=connection,
+        if_exists=if_table_exists,
+        index=False,
+        dtype=column_name_to_sqlalchemy_type,  # type: ignore
     )
-
-    for col in columns_with_list:
-        split_column_str_to_list(
-            uri=connection,
-            schema_name=schema_name,
-            table_name=table_name,
-            in_column=f"{col}_strjoinedAOIFDSIUH",
-            out_column=col,
-            separator="/@#DSLKF",
-            pg_element_type=polars_datatype_to_postgresql_type(col_to_inner_dtype[col]),
-        )
